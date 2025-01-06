991859f2a6608fd6e7331e6d1cae8dbbe849a2ef
Sergey-McArrow <s.makarov.de@gmail.com>
2025-01-06 12:46:35 +0100

    reset

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,16 +3,16 @@ import { authConfig } from './auth.config'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  cookies: {
-    sessionToken: {
-      name: `${authConfig.cookies?.sessionToken?.name}`,
-      options: {
-        ...authConfig.cookies?.sessionToken?.options,
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
-    },
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name: `${authConfig.cookies?.sessionToken?.name}`,
+  //     options: {
+  //       ...authConfig.cookies?.sessionToken?.options,
+  //       httpOnly: true,
+  //       sameSite: 'lax',
+  //       path: '/',
+  //       secure: process.env.NODE_ENV === 'production',
+  //     },
+  //   },
+  // },
 })
